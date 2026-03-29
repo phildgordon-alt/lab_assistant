929d1152a9fa1e97aaf3d2e62b9f6438dd88e0d7
Phil Gordon <phildgordon@gmail.com>
2026-03-29 14:41:08 -0700

    fix: check all possible item array keys

--- export_uncategorized.js
+++ export_uncategorized.js
@@ -1,5 +1,6 @@
 const d = require('/tmp/recon_export.json');
-const items = (d.items || []).filter(i => i.category === 'Uncategorized');
+const all = d.items || d.discrepancies || d.allItems || [];
+const items = all.filter(i => i.category === 'Uncategorized');
 const lines = ['sku,name,kardex_qty,netsuite_qty,variance'];
 for (const i of items) {
   const name = (i.name || '').replace(/"/g, '').replace(/,/g, ' ');
@@ -7,4 +8,5 @@ for (const i of items) {
 }
 const outPath = require('os').homedir() + '/Desktop/uncategorized_skus.csv';
 require('fs').writeFileSync(outPath, lines.join('\n'));
-console.log(items.length + ' SKUs exported to ' + outPath);
+console.log('Total items in response: ' + all.length);
+console.log(items.length + ' uncategorized SKUs exported to ' + outPath);
