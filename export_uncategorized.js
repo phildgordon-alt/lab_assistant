1756ae5187221a903273e69a639a150ff74f78f5
Phil Gordon <phildgordon@gmail.com>
2026-03-29 14:28:32 -0700

    temp: export script for uncategorized SKUs

---
+++ export_uncategorized.js
@@ -0,0 +1,9 @@
+const d = require('/tmp/recon_export.json');
+const items = (d.items || []).filter(i => i.category === 'Uncategorized');
+const lines = ['sku,name,kardex_qty,netsuite_qty,variance'];
+for (const i of items) {
+  const name = (i.name || '').replace(/"/g, '').replace(/,/g, ' ');
+  lines.push(i.sku + ',' + name + ',' + (i.itempath || 0) + ',' + (i.netsuite || 0) + ',' + (i.diff || 0));
+}
+require('fs').writeFileSync('/Users/Shared/lab_assistant/uncategorized_skus.csv', lines.join('\n'));
+console.log(items.length + ' SKUs exported to /Users/Shared/lab_assistant/uncategorized_skus.csv');
