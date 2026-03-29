54543aa773fddba77ddba1c4eba533ba66b4ee33
Phil Gordon <phildgordon@gmail.com>
2026-03-29 14:33:22 -0700

    fix: use homedir for export path

--- export_uncategorized.js
+++ export_uncategorized.js
@@ -5,5 +5,6 @@ for (const i of items) {
   const name = (i.name || '').replace(/"/g, '').replace(/,/g, ' ');
   lines.push(i.sku + ',' + name + ',' + (i.itempath || 0) + ',' + (i.netsuite || 0) + ',' + (i.diff || 0));
 }
-require('fs').writeFileSync('/Users/phil/Desktop/uncategorized_skus.csv', lines.join('\n'));
-console.log(items.length + ' SKUs exported to /Users/phil/Desktop/uncategorized_skus.csv');
+const outPath = require('os').homedir() + '/Desktop/uncategorized_skus.csv';
+require('fs').writeFileSync(outPath, lines.join('\n'));
+console.log(items.length + ' SKUs exported to ' + outPath);
